8da95c0a69cdc9f50cd5748e7d62c59fb35e93af
Ben Ivey <benjaminivey00@gmail.com>
2020-09-21 19:15:03 -0600

    first commit and got the required stuff in

---
+++ index.js
@@ -0,0 +1,21 @@
+const imquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
